196268f291cb0771cd797457a6c66ed0a210d7d1
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2015-01-02 19:58:02 +0100

    Fix microzed init.tcl script

--- hdl/syn/microzed_0/init.tcl
+++ hdl/syn/microzed_0/init.tcl
@@ -1,4 +1,2 @@
-set_param project.boardPartRepoPaths.editable true
-set_param project.boardPartRepoPaths ../../modules/AvnetBoards
-set_property board_part em.avnet.com:microzed_7010_part0:1.0 [current_project]
+set_property board_part em.avnet.com:microzed_7010:part0:1.0 [current_project]
 
