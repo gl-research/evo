890d64ef3d3ebe1b4afbc4b921583440d3c4c05d
Javier D. Garcia-Lasheras <javier@garcialasheras.com>
2015-01-02 16:32:51 +0100

    Microzed HDLMake Manifest.py

---
+++ hdl/syn/microzed_0/Manifest.py
@@ -0,0 +1,14 @@
+target = "xilinx"
+action = "synthesis"
+syn_device = "xc7z010"
+syn_grade = "-1"
+syn_package = "clg400"
+syn_top = "mz_soc_wrapper"
+syn_project = "microzed"
+syn_tool = "vivado"
+
+files = [
+    "../../modules/mz_soc/mz_soc.bd",
+    "../../modules/mz_soc/hdl/mz_soc_wrapper.vhd"
+]
+
